d1b13254b763f9e4f9b31b4c368795406ec2d317
Lucrecious <luca.duran@hotmail.com>
2020-02-03 23:26:09 -0500

    Use command method

--- terminal.gd
+++ terminal.gd
@@ -7,13 +7,23 @@ var history := []
 var history_max = 11
 
 func _ready():
-	command.connect("text_entered", self, "_command")
+	command.connect("text_entered", self, "_pressed_enter")
 
-func _command(text : String) -> void:
-	history.append(text)
+func _pressed_enter(text : String) -> void:
+	var result = _command(text)
+	
+	history.append(result)
 	if history.size() >= history_max:
 		history.pop_front()
 	
 	output.text = LutUtils.join(history, "\n")
 	
 	command.clear()
+
+func _command(command : String) -> String:
+	return "Error!"
+
+
+
+
+
