429f7cb177601e9996650f4fe89b7c2264c9cce5
Lucrecious <luca.duran@hotmail.com>
2020-02-04 22:47:31 -0500

    Remove print statement

--- editor.gd
+++ editor.gd
@@ -5,7 +5,6 @@ class_name GameEditor
 onready var _terminal := $HUD/Terminal
 
 func _on_Terminal_command_entered(command : Dictionary) -> void:
-	print(command)
 	var cmd = command['cmd']
 	if cmd == null: return
 	
