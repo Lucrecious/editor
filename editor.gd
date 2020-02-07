acc84a5d51fae00cf77e770fc4112b1b68e2b409
Lucrecious <luca.duran@hotmail.com>
2020-02-07 00:21:33 -0500

    Add fsm for editor controls

--- editor.gd
+++ editor.gd
@@ -9,17 +9,26 @@ onready var _regions := $Space/Regions as EditorRegions
 onready var _view := $Space/View as EditorView
 onready var _grid := $Space/Grid as EditorGrid
 
+var fsm := FSM.new()
+
+var _event_current
+
 var _selected := []
+
+func _ready() -> void:
+	_grid.set_view(_view.get_rect2())
+	fsm.add_transition(state_idle, state_move_view, to_move_view)
+	fsm.add_transition(state_move_view, state_idle, from_move_view)
+	
+	fsm.state(state_idle)
 
 func get_selected() -> Array:
 	return _selected.duplicate()
 
 func _unhandled_input(event):
-	if event.is_action("editor_select") and\
-		Input.is_action_just_pressed("editor_select"):
-		var add := Input.is_action_pressed("editor_modifier")
-		_select(add)
-		
+	_event_current = event
+	fsm.update(get_physics_process_delta_time())
+
 func _select(add : bool) -> void:
 	if not add:
 		_selected.clear()
@@ -57,7 +66,36 @@ func _get_create_position(location : String) -> Vector2:
 		
 	var default := _view.to_world(Vector2(.756, .51))
 	return _grid.to_coords(default)
+
+var state_idle := FSMQuickState.new(fsm)\
+	.add_main(self, "state_idle_main")
+func state_idle_main() -> void:
+	if Input.is_action_just_pressed("editor_zoom_in"):
+		_view.magnify(1.3)
+	elif Input.is_action_just_pressed("editor_zoom_out"):
+		_view.shrink(1.3)
+	elif Input.is_action_just_pressed("editor_select"):
+		var add := Input.is_action_pressed("editor_modifier")
+		_select(add)
+
+var state_move_view := FSMQuickState.new(fsm)\
+	.add_main(self, "_state_move_view_main")
+func _state_move_view_main() -> void:
+	if _event_current is InputEventMouseMotion:
+		var rel = (_event_current as InputEventMouseMotion).relative
+		_view.global_position -= Vector2(rel.x, rel.y) * _view.get_zoom()
 	
+	_grid.set_view(_view.get_rect2())
+
+var to_move_view := FSMQuickTransition.new(fsm)\
+	.set_evaluation(self, "_to_move_view_evaluation")
+func _to_move_view_evaluation() -> bool:
+	return Input.is_action_just_pressed("editor_alt")
+
+var from_move_view := FSMQuickTransition.new(fsm)\
+	.set_evaluation(self, "_from_move_view_evaluation")
+func _from_move_view_evaluation() -> bool:
+	return Input.is_action_just_released("editor_alt")
 
 
 
